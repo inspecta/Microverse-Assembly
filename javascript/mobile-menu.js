8fa224e0f8ee832c374bc72340e23ea9eb1b67f1
derrick <djmulinde17@gmail.com>
2022-09-13 20:46:21 +0300

    Add mobile menu display using javascript

---
+++ javascript/mobile-menu.js
@@ -0,0 +1,17 @@
+const hamburger = document.querySelector('.hamburger-button')
+const mobileOverlay = document.querySelector('.menu-overlay')
+const menuList = document.querySelector('.menu-list-mobile')
+const closeMobileMenu = document.querySelector('.close-menu-button')
+
+hamburger.addEventListener('click', () => {
+  hamburger.style.display = 'none';
+  mobileOverlay.style.display = 'block';
+  menuList.style.display = 'block';
+});
+
+closeMobileMenu.addEventListener('click', () => {
+  hamburger.style.display = 'block';
+  mobileOverlay.style.display = 'none';
+  menuList.style.display = 'none';
+});
+
