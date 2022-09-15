bb76e82252f47d9571129878e0c4f3d49e16b948
derrick <djmulinde17@gmail.com>
2022-09-15 14:38:09 +0300

    Personalize capstone with own information

--- javascript/activePage.js
+++ javascript/activePage.js
@@ -2,6 +2,8 @@ const activePage = window.location.pathname;
 const navLinks = document.querySelectorAll('nav a');
 
 navLinks.forEach(link => {
+  console.log(link)
+  console.log()
   if (link.href.includes(`${activePage}`)) {
     link.classList.add('active');
   }
