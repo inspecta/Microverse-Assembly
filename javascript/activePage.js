2d4af03abc08cde851ff9bcc7b61561f6d8739cd
derrick <djmulinde17@gmail.com>
2022-09-16 02:26:34 +0300

    Fix linter errors to finalize Capstone

--- javascript/activePage.js
+++ javascript/activePage.js
@@ -2,8 +2,6 @@ const activePage = window.location.pathname;
 const navLinks = document.querySelectorAll('nav a');
 
 navLinks.forEach(link => {
-  console.log(link)
-  console.log()
   if (link.href.includes(`${activePage}`)) {
     link.classList.add('active');
   }
