2df2028aef0d608a9588ad3393e3b5b89e5e860f
Derrick Mulinde <djmulinde@gmail.com>
2022-11-20 18:22:48 +0300

    Update speakers.js

--- javascript/speakers.js
+++ javascript/speakers.js
@@ -65,7 +65,7 @@ speakers.forEach((i) => {
   speakerContainerMain.appendChild(speakerContainer);
 
   const speakerImg = document.createElement('div')
-  speakerImg.className = 'speaker-photo ' + `${speakers[speakers.indexOf(i)].photo}`
+  speakerImg.className = 'speaker-photo ' + `${i.photo}`
   speakerContainer.appendChild(speakerImg);
 
   const speakerDetails = document.createElement('div')
@@ -79,12 +79,12 @@ speakers.forEach((i) => {
   const speakerName = document.createElement('li')
   speakerName.classList.add('speaker-name')
   speakerDetails2.appendChild(speakerName)
-  speakerName.appendChild(document.createTextNode((speakers[speakers.indexOf(i)]).name))
+  speakerName.appendChild(document.createTextNode(i.name))
 
   const speakerHonours = document.createElement('li')
   speakerHonours.classList.add('speaker-honours')
   speakerDetails2.appendChild(speakerHonours)
-  speakerHonours.appendChild(document.createTextNode((speakers[speakers.indexOf(i)]).honours))
+  speakerHonours.appendChild(document.createTextNode(i.honours))
 
   const separatorIcon = document.createElement('li')
   separatorIcon.classList.add('speaker-icon')
@@ -96,5 +96,5 @@ speakers.forEach((i) => {
   const speakerAchievements = document.createElement('li')
   speakerAchievements.classList.add('speaker-achievements')
   speakerDetails2.appendChild(speakerAchievements)
-  speakerAchievements.appendChild(document.createTextNode((speakers[speakers.indexOf(i)]).achievements))
+  speakerAchievements.appendChild(document.createTextNode(i.achievements))
 });
